0e2a7caa9e64e48506c5a9f64f058b8f120ce7f6
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2024-09-24 21:55:46 +0530

    Time: 29 ms (51.79%) | Memory: 26.3 MB (20.66%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-      // pehle iterate the loop and create temp val that store each string   
-      //then sort the temp
-      //now store the temp as a key(as temp is sorted) and its value all the anagram that present in
-      //string
-      unordered_map<string,vector<string>>mp;
-      vector<vector<string>>ans;
-      for(auto str:strs) {
-    string temp=str;
+   
+    
+    string generate(string &s){
+        int count[26]={0};
+    for(auto ch:s)
+    {
+        count[ch-'a']++;
+    }
+     string  new_s=" ";
+    for(int i=0;i<26;i++){
+      
+        if(count[i]>0){
+        new_s+=string(count[i],i+'a');
+        }
+    }
+    
+     return new_s;
+    }
+    
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    
+    unordered_map<string,vector<string>>mp;
+    
+    
+    
+    // iterate the loop over the strs
+         for(auto s:strs){
+     string temp=s; // s is original srings ,jo changes krenge wo temp me krenge jisse 
+                        //jb original use krna ho to s se use kr le
+    string new_s=generate(s);
+    mp[new_s].push_back(s);
+    }  //yaha pr newword jo nikla h wo key h jo original string ko store kra h
+    vector<vector<string>>ans;
+   //av map me ietarte krke second wali mtlb orinal values ko push kro 
+   for(auto it:mp){
+   ans.push_back(it.second);
 
-      sort(begin(temp),end(temp));
-      mp[temp].push_back(str);
-      }
-      for(auto it:mp){
-      ans.push_back(it.second);}
-      return ans;
-    } 
+   }
+   return ans;
+
+    }
 };
