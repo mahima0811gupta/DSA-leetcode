09ebb4b01da0c3d30d4c14dcb8c3e633590278bd
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2024-12-04 22:04:27 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (16.06%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -1,33 +1,42 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        stack<TreeNode*>st;
-        vector<int>ans;
-       TreeNode*temp=root;
-       while(st.size()>0 || temp!=nullptr){
-          if(temp!=nullptr){
-            st.push(temp);
-            temp=temp->left;
-          }
-          else{                // matlb temp ka left null ho gya h 
-            temp=st.top();
-            st.pop();
-            ans.push_back(temp->val);
-            temp=temp->right;
-          }
+      vector<int>ans;
+      TreeNode*curr=root;
+      while(curr!=nullptr){
+     if(curr->left!=nullptr){
+        // find pred
+        TreeNode*pred=curr->left;
+          while(pred->right!=nullptr && pred->right!=curr){
+            pred=pred->right;
+        }
 
-       }
-       return ans;
+        if(pred->right==nullptr) //link kro
+
+        {
+            pred->right=curr;
+            curr=curr->left;
+
+        }
+        if(pred->right==curr) // yaha unlink kroo
+        {
+         pred->right=nullptr;
+         ans.push_back(curr->val);
+          curr=curr->right;
+        }
+        
+     }
+
+     
+     else
+     {
+        ans.push_back(curr->val);
+        curr=curr->right;
+
+     }
+
+      }  
+      return ans;
     }
 };
