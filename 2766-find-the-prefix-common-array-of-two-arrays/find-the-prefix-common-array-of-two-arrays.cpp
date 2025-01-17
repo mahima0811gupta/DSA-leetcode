4ab62e35526019e561a527b1dd718ccb06ca0691
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-01-17 22:46:40 +0530

    Time: 27 ms (7.81%) | Memory: 85.7 MB (92.06%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
          int n=A.size();
+
+         // he brute force approach iterates over each prefix of the arrays A and B and checks if elements in A    are present in B up to the current index. This involves nested loops, leading to a time complexity of O(n3). The idea is straightforward but inefficient due to repeated comparisons.
+
         vector<int>result(n);
         for(int i=0;i<n;i++) {
         int count=0;
