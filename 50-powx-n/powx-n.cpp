b439d7799653efbbf8ac0767e5b23cf42e9514b2
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-01-29 19:30:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (71.98%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double myPow(double x, long n) {
+        if(n==0) return 1;
+        // n -ve
+        if(n<0)
+        return myPow(1/x,-n);
+        // even
+        if(n%2==0)
+        return myPow(x*x,n/2);
+        else
+         return (x*myPow(x*x,(n-1)/2));
+    }
+};
