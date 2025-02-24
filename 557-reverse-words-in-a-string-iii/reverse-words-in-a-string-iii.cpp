82aedc820bf15719477fa882788e639429f36ab9
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-02-25 00:14:22 +0530

    Time: 1 ms (64.20%) | Memory: 16.4 MB (23.15%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream SS(s);
+          string token="";
+          string result="";
+          while(SS>>token){
+              
+              reverse(begin(token),end(token));
+              result+=token+" ";
+
+          }
+            return result.substr(0,result.length()-1);
+
+
+        
+    }
+};
