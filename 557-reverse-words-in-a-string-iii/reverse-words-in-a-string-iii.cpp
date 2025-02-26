28e4d18829f18dc30dc10df3d7381dcedffbb8cb
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-02-26 15:31:24 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (23.34%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     string reverseWords(string s) {
         stringstream SS(s);
-          string token="";
-          string result="";
-          while(SS>>token){
-              
-              reverse(begin(token),end(token));
-              result+=token+" ";
+        string token="";
+        string result="";
+         
+         while(SS>>token){
+          reverse(begin(token),end(token));
 
-          }
-            return result.substr(0,result.length()-1);
+          result+=token+" ";
 
+         }
 
-        
+         return result.substr(0,result.length()-1);
     }
 };
