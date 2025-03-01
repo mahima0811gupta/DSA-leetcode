e869a8c226211388990f04411cb3d22025d05009
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-03-01 16:55:57 +0530

    Time: 4 ms (69.45%) | Memory: 11.5 MB (65.35%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
-
-    bool allzeros(vector<int>&cnt){
-         for(auto i:cnt){
-             if(i!=0)
-             return false;
-         }
-        return true;
-     }
     vector<int> findAnagrams(string s, string p) {
+        int n=p.length();
+        int m=s.length();
+           vector<int>ans;
+        vector<int>cntp(26,0);
+        vector<int>cnts(26,0);
 
-         int n=p.size();
-	    int m=s.size();
-	    vector<int>cnt(26,0);
-for(int i=0;i<n;i++){
-    int ch=p[i];
-    
-    cnt[ch-'a']++;
-    
-}	  
-
-int i=0,j=0;
-vector<int>ans;
-while(j<m){
-    cnt[s[j]-'a']--;
-    
-    if(j-i+1==n){
-        if(allzeros(cnt))
-        {
-        ans.push_back(i);
+        for(auto &ch:p){
+            cntp[ch-'a']++;
         }
-        cnt[s[i]-'a']++;
-        i++;
-    }
-    j++;
-}
-return ans;        
+
+        int i=0;
+        int j=0;
+        while(j<m){
+            cnts[s[j]-'a']++;
+
+            if(j-i+1>n){
+                cnts[s[i]-'a']--;
+                i++;
+            }
+
+            if(cnts==cntp){
+                ans.push_back(i);
+            }
+        j++;
+        }
+
+        return ans;
+
+
     }
 };
