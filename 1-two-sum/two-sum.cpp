c5b9b11cab6c79a36fe1db67dbb9ec746d2954ce
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-03-03 12:05:03 +0530

    Time: 6 ms (51.33%) | Memory: 16.1 MB (7.05%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        for(int i = 0; i < nums.size()-1; i++){
-            for(int j = i+1; j < nums.size(); j++){
-                if(nums[i] + nums[j] == target && i != j){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
-                
-            }
 
+///using hashmap in 0(n) time complexity...  we store key and index pair in map
+    vector<int> twoSum(vector<int>& nums, int target) {
+   vector<int>ans;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            int rem=target-nums[i];
+            if(!mp.empty() && mp.find(rem)!=mp.end()){
+                 ans.push_back(i);
+            ans.push_back(mp[rem]);
+            }  
+        else
+        // hai equal
+        {
+             mp[nums[i]]=i;
+        }
 
         }
-        return ans;
-        
+return ans;
     }
 };
