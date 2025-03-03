77ecbdacb2b95019ce40a827296ada27979d3353
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-03-03 12:14:51 +0530

    Time: 3 ms (15.47%) | Memory: 12 MB (54.17%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+
+
+/// pehle map me key val pair dal ho then ek set bnano usme m.second only occurance dalo agr wo pehle se prensent h set me mtlb repetion hota return false else dal do set me
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int,int>mp;
         for(auto it:arr){
