45085d15e364b7e922c133af7b54b10082e2a553
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-03-04 13:27:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.63%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
-    double myPow(double x, long n) {
-        if(n==0) return 1;
-        if(n<0) 
-        return myPow(1/x,-n);
-        //even
-        if(n%2==0) 
-        return myPow(x*x,n/2);
-        
-        return x*myPow(x*x,(n-1)/2);
+    double myPow(double x, int n) {
+
+        if(x==1)   return 1;
+        if(x==0)      return 0;
+
+        long binaryform=n;
+        double ans=1;
+              //n is negative h to   x%^-10 ko (1/x)^-(-10)  x=1/x and binaryform ko -binaryform
+
+              if(n<0)  {
+                x=1/x;
+                binaryform=-binaryform;
+              }
+        while(binaryform>0){
+             /// are binary form ka last bit 1 aata h to ans me add krenge 5 ka                     binary form 101 hota h 5%2==1 aaya ans me x add krnge usse baad x ko x ze multiple lr denge 
+ if(binaryform%2==1) {
+         ans*=x;
+ }
+ x*=x;
+ binaryform/=2;
+        }
+
+
+
+       return ans; 
     }
 };
