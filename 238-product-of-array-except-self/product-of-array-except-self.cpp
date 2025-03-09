11648e3c8aa902bf6eb79fb0d3578bf848d980d2
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-03-09 23:58:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (58.74%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -3,8 +3,6 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
         vector<int>prev(n);
-        vector<int>suff(n);
-        vector<int>ans(n);
 
         /// finding pre
 
@@ -14,18 +12,14 @@ public:
             prev[i]=p;
             p*=nums[i];
         }
-
         /// fining
          p=nums[n-1];
-         suff[n-1]=1;
          for(int i=n-2;i>=0;i--){
-            suff[i]=p;
+            prev[i]*=p;
             p*=nums[i];
          }
-          for(int i=0;i<n;i++){
-            ans[i]=prev[i]*suff[i];
-          }
+     
         
-        return ans;
+        return prev;
     }
 };
