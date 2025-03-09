2b855c22f8764a2dc9b4a5919e014e5c6b5d3d84
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-03-09 17:06:38 +0530

    Time: 3 ms (30.41%) | Memory: 28.3 MB (6.10%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,15 +3,15 @@ public:
     int majorityElement(vector<int>& v) {
         int n = v.size();
 
-    //declaring a map:
+  
     unordered_map<int, int> mpp;
 
-    //storing the elements with its occurnce:
+    
     for (int i = 0; i < n; i++) {
         mpp[v[i]]++;
     }
 
-    //searching for the majority element:
+    
     for (auto it : mpp) {
         if (it.second > (n / 2)) {
             return it.first;
