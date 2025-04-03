edb167ddbaa11736dd8ec79609f4179ea4f80886
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-04-03 15:59:24 +0530

    Time: 4 ms (0.21%) | Memory: 14.8 MB (14.79%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -12,30 +12,55 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        //base case
-        if(p==nullptr && q==nullptr)
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
+        if(p==NULL&&q!=NULL){
+            return false;
+        }
+        if(p!=NULL&&q==NULL){
+            return false;
+        }
+        if(p==NULL&&q==NULL){
+            return true;
+        }
+        q1.push(p);
+        q2.push(q);
+        while(!q1.empty()&&!q2.empty()){
+            TreeNode* r1=q1.front();
+            TreeNode* r2=q2.front();
+            q1.pop();
+            q2.pop();
+            if(r1->val!=r2->val) return false;
+            if(r1->left!=NULL){
+                if(r2->left==NULL) return false;
+                else{
+                    q1.push(r1->left);
+                    q2.push(r2->left);
+                }
+            }
+            if(r2->left!=NULL){
+                if(r1->left==NULL) return false;
+                else{
+                    q1.push(r1->left);
+                    q2.push(r2->left);
+                }
+            }
+            if(r2->right!=NULL){
+                if(r1->right==NULL) return false;
+                else{
+                    q1.push(r1->right);
+                    q2.push(r2->right);
+                }
+            }
+            if(r1->right!=NULL){
+                if(r2->right==NULL) return false;
+                else{
+                    q1.push(r1->right);
+                    q2.push(r2->right);
+                }
+            }
+        }
         return true;
-         if (p == nullptr || q == nullptr)
-         return false;
-         
-
-
-
-        if(p->val!=q->val)
-        return false;
-        bool lefttree=isSameTree(p->left,q->left);
-        if(lefttree==false)
-        return false;
-        bool righttree=isSameTree(p->right,q->right);
-        if(righttree==false)
-        return false;
-        
-        return true;
-
-
-
-
-
 
     }
 };
