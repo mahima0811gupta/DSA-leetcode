2961185c528a284144763a87e4fd2969bb9a5075
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-04-04 17:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (43.56%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-         int m=matrix.size(),n=matrix[0].size();  
-        int first=0,last=m*n-1;
-        while(first<=last){
-           int mid=first+(last-first)/2;
-           int i=mid/n;
-           int j=mid%n;
 
-           if(matrix[i][j]==target) {
-           return true;
-           }
-           else if(matrix[i][j]<target)
-            first++;
-             else
-             last--;
-        }
-        return false;
+ bool searchincol(vector<vector<int>>& mat, int target,int mid){
+
+    // av row to pta h us row ke col me dekho
+         int m=mat.size();     //row
+        int n=mat[0].size();  
+      int startcol=0;
+      int endcol=n-1;
+
+      while(startcol<=endcol){
+           int colmid=startcol+(endcol-startcol)/2;
+
+           if(mat[mid][colmid]==target)  return true;
+
+           else if(mat[mid][colmid]>target)  endcol=colmid-1;
+
+           else
+           startcol=colmid+1;
+      }
+
+          return false;
     }
-}; 
+ bool searchMatrix(vector<vector<int>>& mat, int target) {
+        /// in this 
+    // first we find that row in which target is exist using binary search and then one that row we apply binary search
+  int m=mat.size();     //row
+  int n=mat[0].size();      //col
+  int startrow=0;
+  int endrow=m-1;
+
+  while(startrow<=endrow){
+    int mid=startrow+(endrow-startrow)/2;
+       if(target>=mat[mid][0] && target<=mat[mid][n-1]){
+          ////correct row is find where target is exist
+             return searchincol(mat,target,mid);
+       }
+       else if(target<mat[mid][0])  endrow=mid-1;
+        else
+        startrow=mid+1;
+
+  }
+    return false;
+    }
+};
