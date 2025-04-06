b5e856e9807e401767c738a7076a86d14c94f6b6
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-04-06 19:25:58 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (99.77%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,39 +11,54 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if (head == nullptr)  
+            return nullptr;
 
-ListNode*fast=head;
-ListNode*slow=head;
-
-for (int i = 0; i < n; i++) {
-            fast = fast->next;
+        // Case: Only one node and we have to delete it
+        if (head->next == nullptr && n == 1) {
+            delete head;
+            return nullptr;
         }
-if (fast == nullptr) {
+
+        // Special case: delete last node
+        if (head->next != nullptr && n == 1) {
             ListNode* temp = head;
-            head = head->next;
-            delete temp;
+            while (temp->next->next != nullptr) {
+                temp = temp->next;
+            }
+            ListNode* end = temp->next;
+            temp->next = nullptr;
+            delete end;
             return head;
         }
-while(fast->next!=nullptr){
-    slow=slow->next;
-     fast = fast->next;
-}
-ListNode*delnode=slow->next;
-slow->next=slow->next->next;
-delete(delnode);
-return head;
 
-   
+        // Count total number of nodes
+        ListNode* temp = head;
+        int cnt = 0;
+        while (temp != nullptr) {
+            temp = temp->next;
+            cnt++;
+        }
 
+        // Case: delete the head node
+        if (n == cnt) {
+            ListNode* nextNode = head->next;
+            delete head;
+            return nextNode;
+        }
 
+        // Find the (cnt - n)th node (node before the one to delete)
+        temp = head;
+        ListNode* prev = nullptr;
+        for (int i = 0; i <cnt - n; i++) {  
+            prev = temp;
+            temp = temp->next;
+        }
 
+        // Delete the nth node from end
+        prev->next = temp->next;
+        delete temp;
 
-
-
-
-
-
-
-
+        return head;
     }
 };
