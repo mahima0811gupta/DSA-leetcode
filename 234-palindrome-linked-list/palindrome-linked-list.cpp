ffdcb1459eed6b08db5af4bd2644c693a01e1bbc
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-04-06 15:00:25 +0530

    Time: 6 ms (35.27%) | Memory: 127.4 MB (29.14%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,18 +11,23 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-      
-        stack<int> stack;
-        ListNode* curr = head;
-        while (curr) {
-            stack.push(curr->val);
-            curr = curr->next;
+        stack<int>st;
+        ListNode*temp=head;
+        while(temp!=nullptr){
+            st.push(temp->val);
+            temp=temp->next;
         }
-        curr = head;
-        while (curr && curr->val == stack.top()) {
-            stack.pop();
-            curr = curr->next;
+        temp=head;
+        while(temp!=nullptr){
+            if(temp->val==st.top()){
+                st.pop();
+                temp=temp->next;
+            }
+            else
+            return false;
         }
-        return curr == nullptr;  
+
+
+        return true;
     }
 };
