9401cf76230c9b94a7beaccbc7b7f87ee53f5e5c
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-04-14 15:28:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (78.94%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-     vector<int>dp;
-    int f(int n,vector<int>& nums ){
-        // base case
-        // sinle home
-        if(n==nums.size()-1)    return nums[n];
-        if(n==nums.size()-2)   return max(nums[n],nums[n+1]);
-        
-          if(dp[n]!=-1)  return dp[n];
 
-          return dp[n]=max(nums[n]+f(n+2,nums),f(n+1,nums));
+      vector<int>dp;
+   int f(int i,vector<int>& nums){
+       /// base case
+       // ek hi gr ho
+       if(i==nums.size()-1)  return nums[i];
 
-          
+       // 2 house ho
+       if(i==nums.size()-2)   return max(nums[i],nums[i+1]);
+     if(dp[i]!=-1)  return dp[i];
+     return dp[i]=max(nums[i]+f(i+2,nums),f(i+1,nums));
 
-    }
-
-
-
-
+   }
     int rob(vector<int>& nums) {
         dp.clear();
-        dp.resize(105,-1);
-        return f(0,nums);
+        dp.resize(405,-1);
+        return f(0,nums); 
     }
 };
