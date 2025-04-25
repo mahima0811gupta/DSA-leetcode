4278f31106a262ec1a2a6f77652fcbbaa1a074e0
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-04-25 11:50:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (89.94%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
+vector<int>dp;
    int f(int n){
     //base case
     if(n==0)  return 1;
     if(n==1)  return 1;
     if(n==2)  return 2;
+    if(dp[n]!=-1)  return dp[n];
       int sum=0;
     for(int k=1;k<=n;k++){
         //here k is potential root ... we will find no of ways too make bst for each root
         sum+=f(k-1)*f(n-k);      //lbt*rbt
     }
-    return sum;
+    return dp[n]=sum;
    }
     int numTrees(int n) {
+        dp.clear();
+        dp.resize(25,-1);
         return f(n);
     }
 };
