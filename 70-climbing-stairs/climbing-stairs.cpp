081637be5d537ec00bbce219d45ce13383025606
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-04-27 18:32:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (71.78%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-   
-   vector<int>dp;
-
+    vector<int>dp;
     int f(int n){
-         
-         if(n==0)  return 1;
+        if(n==0)   return 1;
+        if(dp[n]!=-1)  return dp[n];
+        int sum=0;
+        if((n-1)>=0)
+        sum=f(n-1);
 
-         if(dp[n]!=-1)  return dp[n];
-         int ways=0;
-         for(int i=1;i<=2;i++){
-            if(n-i>=0)
-             ways+=f(n-i);
-         }
+        if((n-2)>=0)
+        sum+=f(n-2);
 
-         return dp[n]=ways;
+         return dp[n]=sum;
     }
+
+
     int climbStairs(int n) {
         dp.clear();
         dp.resize(50,-1);
-
         return f(n);
     }
 };
