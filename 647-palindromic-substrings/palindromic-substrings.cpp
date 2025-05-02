0a4e15780e4937488ec79a9dcb7b3dd50c0f7037
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-05-02 16:51:01 +0530

    Time: 32 ms (25.45%) | Memory: 11 MB (59.09%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    /// time c=n3
-    int dp[1001][1001];
-    bool checkpalindrome(int i,int j,string &s){
-      //base case
-      if(i>j)   return true; /// sbcheck kr liya
-      if(dp[i][j]!=-1)   return dp[i][j];
-      if (s[i]==s[j])  return dp[i][j]=checkpalindrome(i+1,j-1,s);
-        
-      return false;
-    }
-     int cnt=0;
+//using bottom up appoach (blue print)
     int countSubstrings(string s) {
         int n=s.length();
-        memset(dp,-1,sizeof(dp));
-        for(int i=0;i<n;i++){
-           
-            for(int j=i;j<n;j++){
-               if (checkpalindrome(i,j,s)==true)   cnt++;
+        int cnt=0;
+        vector<vector<bool>>dp(n+1,vector<bool>(n+1,false));
+        for(int L=1;L<=n;L++){
+            for(int i=0;i+L-1<n;i++){
+                int j=i+L-1;
+/// 1 len
+                if(i==j)    dp[i][j]=true;
+                else if(i+1==j)   //2 len
+                dp[i][j]=s[i]==s[j];
+                else {
+                    dp[i][j]=(s[i]==s[j] && dp[i+1][j-1]);
+                }
+
+                if(dp[i][j]==true) cnt++;
             }
         }
 
-
-        return cnt;
+return cnt;
     }
 };
