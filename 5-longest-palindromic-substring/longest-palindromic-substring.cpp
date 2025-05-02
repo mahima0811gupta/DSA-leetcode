3f3bbb72711723e9706c4cf4af7d441f7ec3e671
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-05-02 21:38:04 +0530

    Time: 7 ms (89.01%) | Memory: 9.4 MB (73.00%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int n = s.length();
-        vector<vector<bool>> t(n, vector<bool>(n, false));
-        int maxL = 1;
-        int start = 0;
-        
-        for(int i = 0; i<n; i++)
-            t[i][i] = true;
-      
-        
-        for(int L = 2; L<=n; L++) {
-            for(int i = 0; i<n-L+1; i++) {
-                int j = i + L - 1;
-                
-                if(s[i] == s[j] && (t[i+1][j-1] || L == 2)) {
-                    t[i][j] = true;
-                    if(L > maxL) {
-                        maxL = L;
-                        start = i;
-                    }
+    string longestPalindrome(string S) {
+         int start = 0;
+        int maxLen = 1;
+
+        int n = S.length();
+        for (int i = 1; i < n; ++i) {
+            // Even length palindrome
+            int l = i - 1;
+            int r = i;
+            while (l >= 0 && r < n && S[l] == S[r]) {
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
                 }
+                l--;
+                r++;
+            }
+            // Odd length palindrome
+            l = i - 1;
+            r = i + 1;
+            while (l >= 0 && r < n && S[l] == S[r]) {
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+                l--;
+                r++; 
             }
         }
-        
-        return s.substr(start, maxL);
+
+        return S.substr(start, maxLen);
     }
 };
