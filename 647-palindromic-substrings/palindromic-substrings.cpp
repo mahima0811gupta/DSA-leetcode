6b040a13d064c0e7cd811da4e87579b4ee4f8dfc
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-05-02 15:58:56 +0530

    Time: 27 ms (31.83%) | Memory: 12.8 MB (45.04%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     /// time c=n3
+    int dp[1001][1001];
     bool checkpalindrome(int i,int j,string &s){
       //base case
       if(i>j)   return true; /// sbcheck kr liya
-      if (s[i]==s[j])  return checkpalindrome(i+1,j-1,s);
+      if(dp[i][j]!=-1)   return dp[i][j];
+      if (s[i]==s[j])  return dp[i][j]=checkpalindrome(i+1,j-1,s);
         
       return false;
     }
      int cnt=0;
     int countSubstrings(string s) {
         int n=s.length();
-
+        memset(dp,-1,sizeof(dp));
         for(int i=0;i<n;i++){
            
             for(int j=i;j<n;j++){
