f32c8596132e69f24a0a5eead1f5eddcd70ba362
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-05-06 17:18:40 +0530

    Time: 25 ms (73.04%) | Memory: 23.3 MB (32.69%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+/// the approach is same as we fine connected component in a graph.. here we cant use unordered_set or map ase we ahve to amke it pair of set and in c++ there is no aany comparator for this. we can use ordered_set to store visited scell but in each iteration it taes 0(logn) TC
+//so my appraoch is traverse the each cell of the grid and if it is watre skip . if we visit the any cell the mark it 0.
+    int numIslands(vector<vector<char>>& grid) {
+        int row=grid.size();
+        int col=grid[0].size();
+        int cc=0;
+        for(int r=0;r<row;r++){
+            for(int c=0;c<col;c++){
+                 /// if cell is water then skip   and if the cell is already visted we make it 0 
+                 if(grid[r][c]=='0')  continue;
+                 //// cell is unvisited land   means we get more connected component
+                 
+                 cc++;
+                 
+               grid[r][c]='0';  // after visiting the particical cell  mark it zero
+                queue<pair<int,int>>q;
+                q.push({r,c});
+               
+               while(!q.empty()){
+                auto curr=q.front();
+                q.pop();
+                ////  go to the all the unvisited member of currnode
+                int currow=curr.first;
+                int curcol=curr.second;
+
+                //check up
+                if(currow-1>=0  && grid[currow-1][curcol]=='1')  
+                   {
+                    q.push({currow-1,curcol});
+                    grid[currow-1][curcol]='0';
+                   }
+                    //check down
+                if(currow+1<row && grid[currow+1][curcol]=='1')  
+                   {
+                    q.push({currow+1,curcol});
+                    grid[currow+1][curcol]='0';
+                   }
+                    //check left
+                if(curcol-1>=0  && grid[currow][curcol-1]=='1')  
+                   {
+                    q.push({currow,curcol-1});
+                    grid[currow][curcol-1]='0';
+                   }
+                    //check right
+                if(curcol+1<col && grid[currow][curcol+1]=='1')  
+                   {
+                    q.push({currow,curcol+1});
+                    grid[currow][curcol+1]='0';
+                   }
+               }
+
+            }
+        }
+
+        return cc;
+    }
+};
