8687f140f291a18127f1b6e7cda1b0848a891f9c
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-06-22 20:46:36 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (18.41%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -11,42 +11,29 @@
  */
 class Solution {
 public:
- int level(TreeNode*root){
-  //base case
-  if(root==nullptr)  return 0;
-  return 1+ max(level(root->left),level(root->right));             //n time
-
-    }
-
-    void nthlevel(TreeNode*root,int curr,int level,vector<int>&v){
-          if(root==nullptr) return;
-        if(curr==level){
-            v.push_back(root->val);
-            return;
-        }
-        nthlevel(root->left,curr+1,level,v);
-          nthlevel(root->right,curr+1,level,v);
-
-    }
-
-    void lOrder(TreeNode*root,vector<vector<int>>&ans){
-        if(root==nullptr)  return;
-        int n=level(root);
-        for(int i=1;i<=n;i++){
-            vector<int>v;
-            nthlevel(root,1,i,v);
-            if(i%2==0){
-          reverse(v.begin(), v.end());
-          ans.push_back(v);
-            }
-            else
-            ans.push_back(v);
-        }
-
-    }
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-         vector<vector<int>>ans;
-        lOrder(root,ans);
-        return ans;
+          vector<vector<int>>ans; //null hAI
+        queue<TreeNode*>q;
+        if(root==nullptr)    return ans;
+         
+         q.push(root);
+         bool lefttoright=true;
+         while(q.size()>0){
+           int n=q.size();
+            vector<int>v(n);
+            for(int i=0;i<n;i++){
+              TreeNode*temp=q.front();
+              q.pop();
+               int indx=lefttoright?i:n-1-i;
+              v[indx]=temp->val;
+              if(temp->left!=nullptr)   q.push(temp->left);
+
+              if(temp->right!=nullptr)   q.push(temp->right);
+            }
+            //direction chnge krne h 
+            lefttoright=!lefttoright;
+            ans.push_back(v);
+         }
+         return ans;
     }
 };
