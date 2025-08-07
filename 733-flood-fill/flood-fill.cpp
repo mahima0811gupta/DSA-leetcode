b927e97fc01a33979cbd4206326a779a9e519896
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-08-07 15:18:27 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (98.97%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image,int currrow,int currcol,int initialcolor,int newcolor){
-        int n=image.size();
-        int m=image[0].size();
-       if(currrow<0 || currrow>=n || currcol<0 || currcol>=m)   return ;
 
-       if(image[currrow][currcol]!=initialcolor)  return;
-      //if the cell you landed is notof initail color
-       image[currrow][currcol]=newcolor;
-       dfs(image,currrow+1,currcol,initialcolor,newcolor);  //down
-        dfs(image,currrow-1,currcol,initialcolor,newcolor);  //up
-        dfs(image,currrow,currcol+1,initialcolor,newcolor);  //right
-        dfs(image,currrow,currcol-1,initialcolor,newcolor);  //left
+    void dfs(vector<vector<int>>& image,int currr,int currc,int initialcolor,int newcolor){
+        int r=image.size();
+        int c=image[0].size();
+        /////if it is out of bound
+        if(currr<0 || currr>=r || currc<0 || currc>=c)   return;
+
+        /// if the that cell is not same of color of initial color
+
+        if(image[currr][currc]!=initialcolor)   return;
+        //if the cell you landed is notof initail color
+       image[currr][currc]=newcolor;
+      //up
+        dfs(image,currr-1,currc,initialcolor,newcolor);
+        //down
+          dfs(image,currr+1,currc,initialcolor,newcolor);
+        //left
+       dfs(image,currr,currc-1,initialcolor,newcolor);
+        //right
+          dfs(image,currr,currc+1,initialcolor,newcolor);
     }
-    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if(image[sr][sc]==color)  return image;  //edge case otherwise stuck in infinite loop
-        dfs(image,sr,sc,image[sr][sc],color);
-         return image;
-
+       //edge case if the starting color is already same as the given new color 
+       //then reutn 
+       if(image[sr][sc]==color)  return image;
+       dfs(image,sr,sc,image[sr][sc],color);
+       return image;
     }
 };
