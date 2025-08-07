2fbbd2a03acdba497e7f2884114d58383a7d8806
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-08-07 16:45:55 +0530

    Time: 0 ms (100.00%) | Memory: 99.1 MB (71.25%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -3,7 +3,7 @@ public:
     int findCenter(vector<vector<int>>& edges) {
         //brure fortce method is traverse the 2d matrix(edges)  the node have maxinum m frequncy have the centre of the graph but the time complexity is V+E
 
-        ///2nd method is we toook to eges {a b} and {c,d} (given size of graph is n<=3)
+        ///2nd method is we toook to eges {a b} and {c,d} (given size of graph is n<=3)    tc=0(1) 
         //notes if a==c ya b==c then centre is either c or d   bcz tum socho ek ege me se koi ek to centre hoga hi na
        
        int a=edges[0][0];
@@ -11,6 +11,6 @@ public:
        int c=edges[1][0];
        int d=edges[1][1];
     
-    return {(a==c || b==c ? c : d)};
+    return{a==c || b==c ? c:d};
     }
 };
