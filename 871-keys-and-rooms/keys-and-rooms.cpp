3a63338afa84a25c1e3a48ef1c395b4dbcfad857
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-08-08 20:16:17 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (74.64%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        //approach-we have one open room .each room have key of some room means by this rrom we can enter in diff room .. its look like a directed graph in which we path from room to other
-        // we can solve this by dfs/bfs
-        // if rooms.size==visited (means if the open all the room ie traverse all the room      return true)
-    
 
- unordered_set<int>visited;
-    //using bfs
-
-    queue<int>q;
-   
-
-    q.push(0);
-    visited.insert(0);
-    while(!q.empty()){
-        int curr=q.front();
-        q.pop();
-        for(auto neighbour:rooms[curr]){
-             if(!visited.count(neighbour)){
-                q.push(neighbour);
-                visited.insert(neighbour);
-        }
-        }
-       
+    void dfs(vector<vector<int>>& rooms,int node, vector<bool>&visited){
+        visited[node]=true;
+        for(auto &neigh:rooms[node]){
+            if(!visited[neigh]){
+                dfs(rooms,neigh,visited);
+            }}
     }
-    return visited.size()==rooms.size();
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n=rooms.size();
+        vector<bool>visited(n,false);
+       
+         dfs(rooms,0,visited);
 
+        for(int i=0;i<n;i++){
+            if(visited[i]==false)
+            return false;
+        }
+
+
+        return true;
     }
 };
