2750a8a3bea840f7dbeb5e575c84c249fe2a4fb9
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-08-11 02:35:47 +0530

    Time: 7 ms (22.14%) | Memory: 12.3 MB (30.63%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -21,29 +21,33 @@ public:
 
 class Solution {
 public:
-    vector<Node*>NodeRegister;
-    void dfs(Node*actual,Node*clone){
-        for(auto neighbor:actual->neighbors){
-             if(NodeRegister[neighbor->val]==nullptr){
-                Node*newnode=new Node(neighbor->val);
-                NodeRegister[newnode->val]=newnode;
-                clone->neighbors.push_back(newnode);
-                dfs(neighbor,newnode);
-             }
-             else
-             {
-               clone->neighbors.push_back(NodeRegister[neighbor->val]); 
-             }
+
+    unordered_map<Node*,Node*>mp;
+    void dfs(Node* node,Node*clone_node){
+        for(Node*neigh:node->neighbors){
+            ////if clone is not nor created yet
+            if(mp.find(neigh)==mp.end()){
+                //then create the new clone node
+                Node*clone=new Node(neigh->val);
+                //also store in map
+                mp[neigh]=clone;
+                //also connect the neigh in clone graph
+                clone_node->neighbors.push_back(clone);
+                dfs(neigh,clone);
+            }
+            else  //if the node is already connected
+            {
+            clone_node->neighbors.push_back(mp[neigh]);
+            }
         }
     }
     Node* cloneGraph(Node* node) {
-        if(node==nullptr)   return nullptr;
-             Node*clone=new Node(node->val);
-             //resize is must
-             NodeRegister.resize(110,nullptr);
-             NodeRegister[clone->val]=clone;
-             dfs(node,clone);
-             return clone;
-       
+        if(node==nullptr)  return NULL;
+          //clone the give new
+          Node*clone_node=new Node(node->val);
+            mp[node]=clone_node;
+
+            dfs(node,clone_node);
+    return clone_node;
     }
 };
