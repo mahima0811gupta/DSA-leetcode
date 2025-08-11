2d6d0d06acb1f3e794f9da203c71aa596e3018ba
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-08-11 13:54:09 +0530

    Time: 4 ms (35.25%) | Memory: 12.2 MB (30.63%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -18,36 +18,30 @@ public:
     }
 };
 */
-
 class Solution {
 public:
-
-    unordered_map<Node*,Node*>mp;
-    void dfs(Node* node,Node*clone_node){
+     unordered_map<Node*,Node*>mp;
+     void dfs(Node*node,Node*clone_node){
         for(Node*neigh:node->neighbors){
-            ////if clone is not nor created yet
-            if(mp.find(neigh)==mp.end()){
-                //then create the new clone node
-                Node*clone=new Node(neigh->val);
-                //also store in map
-                mp[neigh]=clone;
-                //also connect the neigh in clone graph
-                clone_node->neighbors.push_back(clone);
-                dfs(neigh,clone);
-            }
-            else  //if the node is already connected
-            {
+          if(mp.find(neigh)==mp.end()){   
+             Node*clone=new Node(neigh->val);
+             mp[neigh]=clone;
+             clone_node->neighbors.push_back(clone);
+             dfs(neigh,clone);
+          }
+          else{
             clone_node->neighbors.push_back(mp[neigh]);
-            }
+          }
+          }
         }
-    }
+  
     Node* cloneGraph(Node* node) {
-        if(node==nullptr)  return NULL;
-          //clone the give new
-          Node*clone_node=new Node(node->val);
-            mp[node]=clone_node;
+        if (node==nullptr) return NULL;
+        Node* clone_node=new Node(node->val);
+        mp[node]=clone_node;
+        dfs(node,clone_node);
 
-            dfs(node,clone_node);
-    return clone_node;
+        return clone_node;
+
     }
 };
