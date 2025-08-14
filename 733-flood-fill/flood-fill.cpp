db692af1651f6769ccf8a3efeeb5bc7a3af28da0
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-08-15 02:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (98.92%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
 
-    void dfs(vector<vector<int>>& image,int currr,int currc,int initialcolor,int newcolor){
+
+    void dfs(vector<vector<int>>& image,int currr,int currc,int currcolor,int newcolor){
         int r=image.size();
         int c=image[0].size();
-        /////if it is out of bound
-        if(currr<0 || currr>=r || currc<0 || currc>=c)   return;
 
-        /// if the that cell is not same of color of initial color
 
-        if(image[currr][currc]!=initialcolor)   return;
-        //if the cell you landed is notof initail color
-       image[currr][currc]=newcolor;
-      //up
-        dfs(image,currr-1,currc,initialcolor,newcolor);
-        //down
-          dfs(image,currr+1,currc,initialcolor,newcolor);
-        //left
-       dfs(image,currr,currc-1,initialcolor,newcolor);
-        //right
-          dfs(image,currr,currc+1,initialcolor,newcolor);
+        if(currr<0 || currr>=r || currc<0 || currc>=c || image[currr][currc]!=currcolor)  return;
+        
+        image[currr][currc]=newcolor;
+
+        dfs(image,currr+1,currc,currcolor,newcolor);
+                dfs(image,currr-1,currc,currcolor,newcolor);
+                dfs(image,currr,currc+1,currcolor,newcolor);
+                        dfs(image,currr,currc-1,currcolor,newcolor);        
+
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-       //edge case if the starting color is already same as the given new color 
-       //then reutn 
-       if(image[sr][sc]==color)  return image;
-       dfs(image,sr,sc,image[sr][sc],color);
-       return image;
+        if(image[sr][sc]==color) return image;
+        dfs(image,sr,sc,image[sr][sc],color);
+        return image;
     }
 };
