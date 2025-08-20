a5fdf1dca2bde9813d8ad58a77795bc798a7f7c8
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-08-21 01:01:59 +0530

    Time: 4 ms (21.36%) | Memory: 41.2 MB (40.87%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -5,25 +5,27 @@ public:
         int n=nums.size();
         vector<int>pre(n);
         vector<int>suf(n);
-         vector<int>ans(n);
+       
+        
 
         pre[0]=1;
         int p=1;
-        suf[n-1]=1;
-       int s=1;
+       
+       
         for(int i=1;i<n;i++){
           pre[i]=p*nums[i-1];
           p=pre[i];
         }
-
+    suf[n-1]=1;
+    p=1;
         for(int i=n-2;i>=0;i--){
-          suf[i]=s*nums[i+1];
-          s=suf[i];
+          suf[i]=p*nums[i+1];
+          p=suf[i];
         }
       
        for(int i=0;i<n;i++){
-        ans[i]=pre[i]*suf[i];
+        pre[i]=pre[i]*suf[i];
        }
-return ans;
+return pre;
     }
 };
