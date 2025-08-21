70e076191b1c02a3813f2cfa6e4fc1ddff6c6404
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-08-22 02:24:02 +0530

    Time: 4 ms (38.64%) | Memory: 25.7 MB (50.74%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
@@ -3,22 +3,20 @@ public:
     int maximumNumberOfStringPairs(vector<string>& words) {
         int n=words.size();
         int cnt=0;
-        unordered_set<string>st;
-
-        /// using one loop,..... if check in set ki uska reverse present ya nhi if present h to use pop kr denge agr nhi h to dal denge
+        set<string>st;
         for(int i=0;i<n;i++){
-           string rev=words[i];
+            string rev=words[i];
+
             reverse(rev.begin(),rev.end());
-            if(!st.empty() && st.find(rev)!=st.end()){   //mil gya set me to cnt++ kro aur erase kr do use set se
-                cnt++;
-                st.erase(rev);
+            if(!st.empty()  && st.find(rev)!=st.end()){
+                  cnt++;
+                  st.erase(rev);
             }
-            else  //nahi h 
-            {
-                st.insert(words[i]);
+            else{
+              st.insert(words[i]);
             }
         }
-       
+
         return cnt;
     }
 };
