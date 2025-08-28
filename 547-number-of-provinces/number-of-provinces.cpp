eb8f08e52d49903fe932845958653cde1236fbd1
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-08-28 23:36:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (97.47%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-  int n;
-    void bfs(int node,vector<vector<int>>&isConnected,vector<bool>&visited){
-          queue<int>q;
-          q.push(node);
-           visited[node]=true;
-           while(!q.empty()){
-            int curr=q.front();
-            q.pop();
-           for(int v=0;v<n;v++){
-                if( isConnected[v][curr]&& !visited[v]){
-                    q.push(v);
-                    visited[v]=true;
-                }
-            }
-           }
+
+    void dfs(int node,vector<bool>&visited,vector<vector<int>>& isConnected,int v){
+        visited[node]=true;
+       for(int j=0;j<v;j++){
+         if(isConnected[node][j]==1 && !visited[j])
+         dfs(j,visited,isConnected,v);
+       }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        /// concept here make the count variable and when we run the dfs function ,then inc cnt+= as it means the connceted path is visited
-          n=isConnected.size();
-        int cnt=0;
-        vector<bool>visited(n,false);
-
-        for(int i=0;i<n;i++){
+        int v=isConnected.size();
+        vector<bool>visited(v,false);
+        int province=0;
+        for(int i=0;i<v;i++){
             if(!visited[i]){
-                bfs(i,isConnected,visited);
-                cnt++;
-               
+                province++;
+                dfs(i,visited,isConnected,v);
             }
         }
-        return cnt;
+
+        return province;
     }
 };
