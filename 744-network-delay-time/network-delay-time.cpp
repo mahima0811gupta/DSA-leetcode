e16fe121f3b9af59a09db1317bcd0d3d90c842a3
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-09-01 21:11:10 +0530

    Time: 100 ms (15.51%) | Memory: 48.1 MB (20.06%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,42 +1,50 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>>adj(n+1);
-        for(auto &e:times){
-            int u=e[0];
-            int v=e[1];
-            int w=e[2];
+        typedef pair<int,int> pii; 
+         vector<vector<pii>>adj(n+1);
+        for(auto edge:times){
+            int u=edge[0];
+            int v=edge[1];
+            int w=edge[2];
             adj[u].push_back({v,w});
+           
+            
+        }
+        vector<int>result(n+1,INT_MAX);
+        
+        priority_queue<pii,vector<pii>,greater<pii>>pq;
+        
+        pq.push({k,0}); //src,cost
+        result[k]=0;
+        
+        while(!pq.empty()){
+             int node=pq.top().first;
+             int cost=pq.top().second;
+             
+             pq.pop();
+             
+             for(auto &neigh:adj[node]){
+                 int adjnode=neigh.first;
+                 int adjcost=neigh.second;
+                       
+                 if(cost+adjcost<result[adjnode]){
+                     result[adjnode]=cost+adjcost;
+                     pq.push({adjnode,cost+adjcost});
+                     
+                 }
+             }
+            
         }
 
-priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-vector<int>result(n+1,INT_MAX);
-result[k]=0;
-pq.push({0,k});
-while(!pq.empty()){
-    int d=pq.top().first;
-    int node=pq.top().second;
-    pq.pop();
-    for(auto &it:adj[node]){
-        int adjnode=it.first;
-        int dist=it.second;
 
-        if(d+dist<result[adjnode]){
-            result[adjnode]=d+dist;
-            pq.push({d+dist,adjnode});
+        int  maxi=INT_MIN;
+        for(int i=1;i<=n;i++){
+            maxi=max(result[i],maxi);
         }
+
+        if(maxi==INT_MAX)   return -1;
+        return maxi;
     }
-}
-  int maxi=INT_MIN;
-for(int i=1;i<=n;i++){
-  if(result[i]>maxi)
-  maxi=result[i];
 
-
-}
-   if(maxi==INT_MAX)   return -1;
-
-   return maxi;
-
-    }
 };
