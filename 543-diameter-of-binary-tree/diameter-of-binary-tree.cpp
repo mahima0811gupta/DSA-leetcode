6c9af922e37c733e7764dd20658d388ededaee71
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-09-05 18:16:57 +0530

    Time: 332 ms (5.02%) | Memory: 24 MB (8.36%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,22 +11,20 @@
  */
 class Solution {
 public:
-  
-    int height(TreeNode*root){
-        if(root==nullptr)            return 0;
-        return 1+max(height(root->left),height(root->right));
 
-    }   //time complexuty of height function is 0(n)
-    int diameterOfBinaryTree(TreeNode* root) {
+    int level(TreeNode*root){
         if(root==nullptr)  return 0;
-        //// diamertre ya to left side ho skta ya right side ya to jo root se hokr .teno me jo maxi hoga wahi hoga
+        return 1+max(level(root->left),level(root->right));
+    }
+    int maxdia=0;
+    int diameterOfBinaryTree(TreeNode* root) {
+         if(root==nullptr)  return 0;
+        int dia=level(root->left)+level(root->right);
+         maxdia=max(dia,maxdia);
+  diameterOfBinaryTree(root->left);
+   diameterOfBinaryTree(root->right);
+ return maxdia;
 
-
-//here here we find 3 function for each node..means time c is n2
-        int left=diameterOfBinaryTree(root->left);
-          int right=diameterOfBinaryTree(root->right);
-          int curr=height(root->left)+height(root->right);
-
-          return max(curr,max(left,right));
+        
     }
 };
