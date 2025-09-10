3acdf9e376cf13c0093614c26c68d10735f9edb2
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-09-10 21:55:31 +0530

    Time: 6 ms (4.85%) | Memory: 18.3 MB (17.55%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -1,24 +1,33 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public:
-    void helper(TreeNode* root,string path,vector<string>&ans){
-            if(root==nullptr)  return;
-            string a=to_string(root->val);
+public: 
 
-            //base case
-            if(root->left==nullptr && root->right==nullptr){
-                path+=a;
-                ans.push_back(path);
-                return;
-            }
+   void helper(TreeNode* root,string path, vector<string>&ans){
+    if(root==NULL)  return;
+      string a=to_string(root->val);
+      if(root->left==NULL && root->right==NULL) {
+        path+=a;
+        ans.push_back(path);
+      }
 
-            helper(root->left,path+a+"->",ans);
-             helper(root->right,path+a+"->",ans);
-    }
+     helper(root->left,path+a+"->",ans);
+     helper(root->right,path+a+"->",ans);
+
+   }
     vector<string> binaryTreePaths(TreeNode* root) {
-     vector<string>ans;
-    helper(root,"",ans);
-    return ans;
-        
+        vector<string>ans;
+        helper(root,"",ans);
+
+return ans;
     }
 };
