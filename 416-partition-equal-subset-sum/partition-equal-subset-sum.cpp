ccaeefd7aa5b97ffffed549a9c1ded6ab2f69c40
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-09-23 20:00:43 +0530

    Time: 79 ms (83.77%) | Memory: 205.4 MB (7.14%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-vector<vector<int>>dp;
-   bool f(vector<int>&nums,int i,int k){
-    /// base case
-    if(k==0)   return true;  //means k keequal subset mil gya
 
-    if(i==nums.size())  return false; //last tk chle gye but k zero nhi hua
-   if(dp[i][k]!=-1)  return dp[i][k];
-    if(nums[i]<=k){  /// exclude or include only possible
-        return dp[i][k]=f(nums,i+1,k-nums[i]) or f(nums,i+1,k);
-    }
-    else{   //only exculde
-   return  dp[i][k]=f(nums,i+1,k);
+    bool subsetsum(vector<int>& nums,int n, int sum, vector<vector<int>>&dp){
+        //basecase
+        if(sum==0) return true;
+        if(sum==0 && n==0)  return true;
+        if(n==0) return false;
+         if(dp[n][sum]!=-1)   return dp[n][sum];
+        if(sum>=nums[n-1]){
+          dp[n][sum]=subsetsum(nums,n-1,sum-nums[n-1],dp) ||subsetsum(nums,n-1,sum,dp); 
+        }
+        else 
+        dp[n][sum]=subsetsum(nums,n-1,sum,dp);
 
-    }
-      }
-    bool canPartition(vector<int>& nums) {
+        return dp[n][sum];
+    } 
+      bool canPartition(vector<int>& nums) {
         int sum=0;
+        int n=nums.size();
+       
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
         }
-        if(sum%2!=0) //odd    
-        return false;
-        dp.clear();
-        dp.resize(205,vector<int>(sum+5,-1));
-        return f(nums,0,sum/2);
+
+        if(sum%2!=0) return false;
+
+         vector<vector<int>>dp(n+1,vector<int>(sum+2,-1));
+        
+        return subsetsum(nums,n,sum/2,dp);
+
     }
 };
