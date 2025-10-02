2d965c9f8b026bd611fbdb28c8667a5a4f50d456
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-10-02 12:38:46 +0530

    Time: 78 ms (12.74%) | Memory: 13.9 MB (93.18%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> ans;
         for(int i = 0; i < nums.size()-1; i++){
             for(int j = i+1; j < nums.size(); j++){
-                if(nums[i] + nums[j] == target && i != j){
+                if(nums[i] + nums[j] == target ){
                     ans.push_back(i);
                     ans.push_back(j);
                     break;
