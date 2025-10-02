3b951e7d4107b617d3ac7f751c546b1eb6b124c6
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-10-02 16:22:05 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (60.84%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        //here we can fisrt we can to buy then can e can sell so 
+        // To find maximum profit, we assume each day as a selling day.
+       // However, we cannot start selling from day 0, because the first day can only be a buying day.
+// For each day, we calculate if selling today would give a higher profit than before.
+// We also keep updating the best buying price so far.
+
+        int n=prices.size();
+        int bestbuys=prices[0];
         int maxprofit=0;
-        int bestbuy=prices[0];
+        for(int i=0;i<n;i++){
+            if(prices[i]>bestbuys)
+              maxprofit=max(maxprofit,prices[i]-bestbuys);
+               bestbuys=min(bestbuys,prices[i]);
+                      }
 
-        for(int i=1;i<prices.size();i++){
-            
-           if (prices[i]>bestbuy){
-            maxprofit=max( maxprofit,prices[i]-bestbuy);
-           }
+                      return maxprofit;
 
-            bestbuy=min( bestbuy,prices[i]);
-
-        }
-       return maxprofit;
     }
 };
