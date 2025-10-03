8d3f113671fbac58cf68d11c35ac7a7cdaabee25
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-10-03 15:29:57 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (36.05%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
-        unordered_map<int,int>mp;    //value--->indx
+        unordered_map<int,int>mp;  //value--->indx
+
         for(int i=0;i<n;i++){
             int a=target-nums[i];
             if(mp.find(a)!=mp.end()){
-                 return {i,mp[a]};
+                return {i,mp[a]};
             }
             else{
                 mp[nums[i]]=i;
             }
         }
-
-       return {};
+        return {};
     }
 };
