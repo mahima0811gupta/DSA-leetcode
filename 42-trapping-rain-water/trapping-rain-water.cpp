4f7fb9bf81868f7888d075966d2456e0cf850c87
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-10-03 13:42:08 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (60.57%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
-
-
-   //tc-3n  sc-n
-//    For any bar i, the amount of water that can be trapped above it depends on:
-// The highest bar on its left (leftMax[i])
-// The highest bar on its right (rightMax[i])
-// The trapped water at index i =
-// min(leftMax[i], rightMax[i]) - height[i]
-
-
-
-
-// We use two prefix arrays:
-// leftMax[i] → stores the tallest bar height to the left of i
-// rightMax[i] → stores the tallest bar height to the right of i
-// Then, in one more loop, we apply the formula for all indices.
-
     int trap(vector<int>& height) {
         int n=height.size();
-        vector<int>lmax(n,0),rmax(n,0);
-        int ans=0;
-        lmax[0]=height[0];
-        rmax[n-1]=height[n-1];
-        for(int i=1;i<n;i++){
-            lmax[i]=max(lmax[i-1],height[i]);
-        }
+        int lmax=height[0];
+        int rmax=height[n-1];
+         int ans=0;
+        int s=0;
+        int e=n-1;
+        while(s<=e){
+            lmax=max(lmax,height[s]);
+            rmax=max(rmax,height[e]);
 
-         for(int i=n-2;i>=0;i--){
-            rmax[i]=max(rmax[i+1],height[i]);
+            if(lmax<rmax)   //deciding factor
+            {
+              ans+=lmax-height[s];
+              s++;
+            }
+            else{
+                ans+=rmax-height[e];
+              e--;
+            }
+
         }
-        
-        for(int i=0;i<n;i++){
-            ans+=min(lmax[i],rmax[i])-height[i];
-        }
-return ans;
+        return ans;
+
     }
 };
