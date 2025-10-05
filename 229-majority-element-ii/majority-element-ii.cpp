3dad0c41302f18390dc755738e5a003b88ee6bfc
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-10-05 15:15:58 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (81.81%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int candidate1 = 0, cnt1 = 0;
-        int candidate2 = 0, cnt2 = 0;
-        int n = nums.size();
-        vector<int> ans;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == candidate1) {
-                cnt1++;
-            } else if (nums[i] == candidate2) {
-                cnt2++;
-            } else if (cnt1 == 0) {
-                candidate1 = nums[i];
-                cnt1 = 1;
-            } else if (cnt2 == 0) {
-                candidate2 = nums[i];
-                cnt2 = 1;
-            } else {
-                cnt1--;
-                cnt2--;
-            }
+        //here maximun two elemenet can occur n/3 times spo we take two candidate;
+        int candidate1=INT_MIN,cnt1=0;
+        int candidate2=INT_MIN,cnt2=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+              if(candidate1==nums[i]) cnt1++;
+             else if(candidate2==nums[i]) cnt2++;
+             else if(cnt1==0){
+                if(candidate2!=nums[i]) {
+                    cnt1++;
+                    candidate1=nums[i];
+                }
+             }
+             else if(cnt2==0){
+                if(candidate1!=nums[i]) {
+                    cnt2++;
+                    candidate2=nums[i];
+                }
+             }
+        
+         else{
+            cnt1--;
+            cnt2--;
+         }
+             
         }
+ vector<int>ans;
+      cnt1=0,cnt2=0;
+         for(int i=0;i<n;i++){
+            if(nums[i]==candidate1) cnt1++;
+             if(nums[i]==candidate2) cnt2++;
+         }
+         if(cnt1>n/3) ans.push_back(candidate1);
+           if(cnt2>n/3) ans.push_back(candidate2);
 
-        int cntt1 = 0, cntt2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == candidate1) cntt1++;
-            if (nums[i] == candidate2) cntt2++;
-        }
+           return ans;
 
-    
-        if (cntt1 > n / 3) ans.push_back(candidate1);
-        if (cntt2 > n / 3 && candidate2 != candidate1) ans.push_back(candidate2);
-
-        return ans;
     }
 };
