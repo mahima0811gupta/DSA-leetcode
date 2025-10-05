49d90db687373d55928c57f84e5aa7c63665b97b
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-10-05 23:25:48 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (99.60%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,50 +1,29 @@
 class Solution {
 public:
-
-// binary search
     int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int l=0, r= n-1;
+        if(n==1) return nums[0];
 
-        int n=nums.size();
-          // if single lemnet h to
-        
-        if(n==1) 
-        return nums[0];
-  // checking f0r 1dst index
-     int i=0;
-     if(nums[i]!=nums[i+1])
-        return nums[i];
-
-      // last ndex
-      if( nums[n-1]!=nums[n-2])
-        return nums[n-1];
-
-
-        int s=1;
-        int e=n-2;
-
-        while(s<=e){
-               
-               int mid=s+(e-s)/2;
-              if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
-
-                     return nums[mid];
-
-
-         //// eliminate left or right part
-         // if mid even pr hoga aur same elemnet odd hoga 
-        // ya mid odd pr hoga same ele prior even pr hogi to left part pr h aursingle value rught part pr h to hme left part lemeninate krna hoga
-
- ///left side
-        if(mid%2==0 && nums[mid]==nums[mid+1] || mid%2==1 && nums[mid]==nums[mid-1]){
-            // elimate the left part
-            s=mid+1;
-
+        while(l<r){
+            int mid = (l+r)/2;
+            if(nums[mid] != nums[mid+1] && nums[mid-1] != nums[mid]){
+                return nums[mid];
+            }
+            if(nums[mid] == nums[mid+1]){
+                if((mid) %2 !=0){
+                    r= mid-1;
+                }else{
+                    l = mid+1 +1;
+                }
+            }else if(nums[mid-1] == nums[mid]){
+                 if((mid) %2 !=0){
+                    l=mid+1;
+                }else{
+                    r= mid-2;
+                }
+            }
         }
-        else
-          e=mid-1;
-
-        }
-
-        return -1;
+        return nums[l];
     }
 };
