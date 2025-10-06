e7dbe3658456f0c01c3014b69aeeb933dea70fb0
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-10-06 19:46:47 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (52.31%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int maxi=INT_MIN;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+
+            if(sum>maxi)  maxi=sum;
+            if(sum<0)
+                  sum=0;
+        }
+        return maxi;
+    }
+};
+// The intuition of the algorithm is not to consider the subarray as a part of the answer if its sum is less than 0. A subarray with a sum less than 0 will always reduce our answer and so this type of subarray cannot be a part of the subarray with maximum sum.
+// Here, we will iterate the given array with a single loop and while iterating we will add the elements in a sum variable. Now, if at any point the sum becomes less than 0, we will set the sum as 0 as we are not going to consider any subarray with a negative sum. Among all the sums calculated, we will consider the maximum one.
+
+// Thus we can solve this problem with a single loop.
