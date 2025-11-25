21cc767962fbf02368d13a9ab9b47c605ad961f3
Mahima Gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-11-25 20:32:04 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (33.36%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -2,20 +2,31 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n=nums.size();
+        if(n==1) return nums[0];
+        //edge case 
+        if(nums[n-2]!=nums[n-1])  return nums[n-1];
+        if(nums[0]!=nums[1])   return nums[0];
+        //starting even se hogi n...ending odd se hogi...
         int s=0;
         int e=n-1;
-        while(s<e){
+        while(s<=e){
             int mid=s+(e-s)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
 
-            if(mid%2!=0)  mid--;
+            else if(nums[mid]==nums[mid-1])  //mid odd pr hona chhaiye  
+            { if(mid%2!=0)  //odd h
+                  s=mid+1;
+               else
+                 e=mid-1;  
+            }
+            else if(nums[mid]==nums[mid+1])    //mid even hoga to sahi h aage badho
+             {
+                if(mid%2==0)  s=mid+1;
+                else e=mid-1;
+             }
+            }
 
-            if(nums[mid]==nums[mid+1])  s=mid+2;
-            else
-            //left
-            e=mid;
-
-        }
-
-        return nums[s];
+            return -1;
+        
     }
 };
