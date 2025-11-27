69798370794e4ddcb899df38723b01755d45a801
Mahima Gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-11-27 17:44:50 +0530

    Time: 4 ms (88.68%) | Memory: 23 MB (44.72%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,34 +1,26 @@
-// Koko must eat all bananas within h hours. The faster she eats, the fewer hours she needs.
-// The key is to notice that the answer lies between 1 and the maximum pile size, and the
-// condition (can she finish in h hours?) is monotonic:
-// If she can finish at speed k, she can also finish at any faster speed.
-// If she cannot finish at speed k, she also cannot finish at any slower speed.
-// This monotonic property suggests binary search on the eating speed.
-
-
 class Solution {
 public:
 
-
-    bool canEatAll(vector<int>& piles,int givenhour,int h){
-        int actualhour=0;
+    bool CanEat(vector<int>& piles,int givenspeed,int h){
+        int actualspeed=0;
         for(auto x:piles){
-         actualhour+=x/givenhour;
-         if(x % givenhour != 0) actualhour++;
+            actualspeed+=x/givenspeed;
+           if(x%givenspeed!=0) actualspeed++;
         }
-
-        return actualhour<=h;
+        return actualspeed<=h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-       int s=1;
-       int e=*max_element(begin(piles),end(piles));
-       while(s<e){
-        int mid=s+(e-s)/2;
-        if(canEatAll(piles,mid,h))  //check piche
-        e=mid;
-        else
-        s=mid+1;
-       }    
-       return s;    
+        int l=1;
+        int r=*max_element(begin(piles),end(piles));
+        while(l<r){
+            int mid=l+(r-l)/2;
+
+            if(CanEat(piles,mid,h))
+            r=mid;
+            else
+            l=mid+1;
+        }
+
+        return l;
     }
 };
